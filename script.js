a1b502aa80a1603e8d01396f3660e233fd1a636a
Steven Kim <sskim14@gmail.com>
2019-10-19 00:29:28 -0400

    Updated style, added background, added api to button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+window.onload = function() {
 const api_key = "SZTkBwjQYYYVremZV8DUaaETiwKktzF2";
-const url = "https://api.giphy.com/v1/gifs/random?api_key=SZTkBwjQYYYVremZV8DUaaETiwKktzF2&tag=&rating=PG";
+const url = "https://api.giphy.com/v1/gifs/random?api_key=SZTkBwjQYYYVremZV8DUaaETiwKktzF2&tag=&rating=G";
 
 fetch(url)
     .then(res => {
@@ -16,4 +17,25 @@ fetch(url)
     .catch(err => {
         console.log("Something went wrong...", err)
     })
-    "background-image: url('img_girl.jpg');"
+
+const nextButton = document.getElementById("next");
+
+nextButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            console.log("Success!", res)
+            console.log(res.data.image_original_url);
+            const giphy = document.querySelector(".giphy");
+            const imgUrl = res.data.image_original_url;
+            const styles = "background-image: url("+imgUrl+");";
+            giphy.style = styles;
+        })
+        .catch(err => {
+            console.log("Something went wrong...", err)
+        })
+})
+}
