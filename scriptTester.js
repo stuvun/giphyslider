87b6d899119feea5bb15ea034f716f38e040e33e
Steven <sskim14@gmail.com>
2019-10-20 02:52:54 -0400

    Update scriptTester.js
    Test 2

--- scriptTester.js
+++ scriptTester.js
@@ -1,32 +1,48 @@
 window.onload = function() {
 
-class Fetch {
-    constructor() {
-        this.giphys = document.querySelectorAll(".slider");
-        this.api_key = "SZTkBwjQYYYVremZV8DUaaETiwKktzF2";
-        this.url = "https://api.giphy.com/v1/gifs/random?api_key="+this.api_key+"&tag=&rating=G";
-    }
+giphys = document.querySelectorAll(".slider");
+buttons = document.querySelectorAll(".buttons");
+api_key = "SZTkBwjQYYYVremZV8DUaaETiwKktzF2";
+url = "https://api.giphy.com/v1/gifs/random?api_key="+api_key+"&tag=&rating=G";
 
-    fetchGiphys() {
-        for (let i = 0; this.giphys.length < 3; i++) {
-            fetch(this.url)
-                .then(res => {
-                    return res.json()
-                })
-                .then(res => {
-                    console.log("Success!", res);
-                    console.log(res.data.image_original_url);
-                    const imgUrl = res.data.image_original_url;
-                    const styles = "background-image: url("+imgUrl+");";
-                    this.giphys[i].style = styles
-                })
-                .catch(err => {
-                    console.log("Something went wrong...", err)
-                })
-        }
+function fetchGiphys() {
+    for (let i = 0; giphys.length < 3; i++) {
+        fetch(url)
+            .then(res => {
+                return res.json()
+            })
+            .then(res => {
+                console.log("Success!", res);
+                console.log(res.data.image_original_url);
+                const imgUrl = res.data.image_original_url;
+                const styles = "background-image: url("+imgUrl+");";
+                giphys[i].style = styles
+            })
+            .catch(err => {
+                console.log("Something went wrong...", err)
+            })
     }
 }
-Fetch = new Fetch;
+fetchGiphys();
 
-Fetch.fetchGiphys();
+
+buttons[0].addEventListener("click", (evt) => {
+    evt.preventDefault();
+    function slides() {
+    fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            console.log("Success!", res)
+            console.log(res.data.image_original_url);
+            const imgUrl = res.data.image_original_url;
+            const styles = "background-image: url("+imgUrl+");";
+            giphys[0].style = styles;
+        })
+        .catch(err => {
+            console.log("Something went wrong...", err)
+        })
+    }
+})
 }
