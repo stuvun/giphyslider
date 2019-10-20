1a4b9a866fc14cfeb3a5a335c19ed8b264422aa6
Steven Kim <sskim14@gmail.com>
2019-10-20 14:45:12 -0400

    Set up second tester js

--- scriptTester.js
+++ scriptTester.js
@@ -6,27 +6,21 @@ const buttons = document.querySelectorAll(".buttons");
 const api_key = "SZTkBwjQYYYVremZV8DUaaETiwKktzF2";
 const url = "https://api.giphy.com/v1/gifs/random?api_key="+api_key+"&tag=&rating=G";
 
-for (let i = 0; i < 3; i++) {
-    fetch(url)
-        .then(res => {
-            return res.json()
-        })
-        .then(res => {
-            console.log("Success!", res);
-            console.log(res.data.image_original_url);
+fetch(url)
+    .then(res => {
+        return res.json()
+    })
+    .then(res => {
+        console.log("Success!", res);
+        console.log(res.data.image_original_url);
 
-            var imgUrl = res.data.image_original_url;
-            let styles = "background-image: url("+imgUrl+");";
-            giphys[i].style = styles;
-
-            if (i === 0) { giphys[0].style.visibility = "visible" }
-            else if (i === 1) {giphys[1].style.visibility = "hidden"}
-            else if (i === 2) {giphys[2].style.visibility = "hidden"}
-        })
-        .catch(err => {
-            console.log("Something went wrong...", err)
-        })
-}
+        let imgUrl = res.data.image_original_url;
+        let styles = "background-image: url("+imgUrl+");";
+        giphys[0].style = styles;
+    })
+    .catch(err => {
+        console.log("Something went wrong...", err)
+    })
 
 // <-- 'PREVIOUS' button
 buttons[0].addEventListener("click", (evt) => {
@@ -38,8 +32,8 @@ buttons[0].addEventListener("click", (evt) => {
         .then(res => {
             console.log("Success!", res)
             console.log(res.data.image_original_url);
-            const imgUrl = res.data.image_original_url;
-            const styles = "background-image: url("+imgUrl+");";
+            let imgUrl = res.data.image_original_url;
+            let styles = "background-image: url("+imgUrl+");";
             
             // The div ending on the right will change its image url and hide | The middle div will show
             // Original order should be [2, 0, 1] --> [1, 2, 0] (after click)
@@ -76,8 +70,8 @@ buttons[1].addEventListener("click", (evt) => {
         .then(res => {
             console.log("Success!", res)
             console.log(res.data.image_original_url);
-            const imgUrl = res.data.image_original_url;
-            const styles = "background-image: url("+imgUrl+");";
+            let imgUrl = res.data.image_original_url;
+            let styles = "background-image: url("+imgUrl+");";
             
             // The div ending on the right will change its image url and hide | The middle div will show
             // Original order should be [2, 0, 1] --> [0, 1, 2] (after click)
