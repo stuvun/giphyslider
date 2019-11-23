edb3241a5db41f2695765ae4763c555ce9026a5b
Steven <sskim14@gmail.com>
2019-11-23 17:17:04 -0500

    Slider functions properly, working on transitions

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let a = 0;
 const buttons = document.querySelectorAll(".button");
 const giphyContainer = document.querySelector(".giphyGallery");
 const giphy = document.querySelectorAll(".image");
 
-const slideWidth = 100;
-const slideCount = 3;
+let a = 0;
 let currentSlide = 1;
 
 const api_key = "SZTkBwjQYYYVremZV8DUaaETiwKktzF2";
@@ -18,42 +16,50 @@ fetch(url)
     })
     .then(res => {
         console.log("Success!", res);
-        giphy.forEach((gif, index) => {
-            let gifPosition = a + index;
+        giphy.forEach((gif) => {
             gif.style = "null;";
-            gif.style = "background-image: url('" + res.data[gifPosition].images.downsized_large.url + "');";
-            console.log(res.data[gifPosition].images.downsized_large.url)
+            gif.style = "background-image: url('" + res.data[a].images.downsized_large.url + "');";
+            console.log(res.data[a].images.downsized_large.url)
         })
         giphySwitchMinus = () => {
-            let delta = currentSlide * slideWidth;
-            giphy.forEach((gif, index) => {
-                let gifPosition = a + index;
+            a = a - 1;
+            giphy.forEach(gif => {
                 gif.style = "null;";
-                gif.style = "background-image: url('" + res.data[gifPosition].images.downsized_large.url + "');";
-                console.log(res.data[gifPosition].images.downsized_large.url)
+                gif.style = "background-image: url('" + res.data[a].images.downsized_large.url + "');";
             })
-            if (currentSlide < slideCount) {
-                giphyContainer.style = `transform: translateX(+${delta}vw);`;
-                currentSlide = currentSlide - 1
-            } else {
-                giphyContainer.style = 'transform: translateX(0vw);';
+            console.log(res.data[a].images.downsized_large.url)
+            if (currentSlide === 1) {
+                giphyContainer.style = 'transform: translateX(2);';
                 currentSlide = 3
+            } else {
+                giphyContainer.style = 'transform: translateX(2);';
+                currentSlide = currentSlide - 1
             }
+            giphyContainer.style = 'transform: translateX(1);';
         }
         giphySwitchPlus = () => {
-            let delta = currentSlide * slideWidth;
-            giphy.forEach((gif, index) => {
-                let gifPosition = a + index;
+            if (a < -1) {
+                a = -1
+            }
+            a = a + 1;
+            giphy.forEach(gif => {
                 gif.style = "null;";
-                gif.style = "background-image: url('" + res.data[gifPosition].images.downsized_large.url + "');";
-                console.log(res.data[gifPosition].images.downsized_large.url)
+                gif.style = "background-image: url('" + res.data[a].images.downsized_large.url + "');";
             })
-            if (currentSlide < slideCount) {
-                giphyContainer.style = `transform: translateX(-${delta}vw);`;
-                currentSlide = currentSlide + 1
-            } else {
-                giphyContainer.style = 'transform: translateX(0vw);';
+            console.log(res.data[a].images.downsized_large.url)
+            if (currentSlide === 3) {
+                giphyContainer.style = 'transform: translateX(0);';
                 currentSlide = 1
+            } else {
+                giphyContainer.style = 'transform: translateX(0);';
+                currentSlide = currentSlide + 1
+            }
+            giphyContainer.style = 'transform: translateX(1);';
+            if (buttons[0].style.visibility == "hidden") { 
+                buttons[0].style.visibility = "null;";
+                buttons[0].style.visibility = "visible"
+            } else {
+                return buttons[0].style.visibility
             }
         }
         console.log(res.data[0].images.downsized_large.url);
@@ -61,28 +67,20 @@ fetch(url)
         buttons[0].addEventListener("click", function(e) {
             e.preventDefault();
             if (a == 1) {
-                a = a - 1;
                 buttons[0].style.visibility = "null;";
                 buttons[0].style.visibility = "hidden";
-                console.log(a);
-                giphySwitchMinus()
+                giphySwitchMinus();
+                console.log(a)
             } else {
-                a = a - 1;
+                giphySwitchMinus()
                 console.log(a)
             }
         })
 
         buttons[1].addEventListener("click", function(e) {
             e.preventDefault();
-            a = a + 1;
-            console.log(a);
             giphySwitchPlus();
-            if (buttons[0].style.visibility == "hidden") { 
-                buttons[0].style.visibility = "null;";
-                buttons[0].style.visibility = "visible"
-            } else {
-                return buttons[0].style.visibility
-            }
+            console.log(a)
         })
     })
     .catch(err => {
